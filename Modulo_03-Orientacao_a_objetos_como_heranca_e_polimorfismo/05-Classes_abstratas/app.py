a25070ef06ffedeb8536043215e0fd9072719b36
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-23 23:34:37 -0300

    Correção de declaração de varíaveis tipo objeto.

--- Modulo_03-Orientacao_a_objetos_como_heranca_e_polimorfismo/05-Classes_abstratas/app.py
+++ Modulo_03-Orientacao_a_objetos_como_heranca_e_polimorfismo/05-Classes_abstratas/app.py
@@ -7,10 +7,10 @@ def main():
 
     # conta_cliente_1 = ContaCliente(1, 0.1, 0.25, 0.1)
 
-    conta_Comum_1 = ContaComum(1, 0.1, 0.01, 2000, 0.03)
+    conta_comum_1 = ContaComum(1, 0.1, 0.01, 2000, 0.03)
     conta_vip_1 = ContaVIP(1, 0.5, 0.05, 4000, 0.10)
 
-    print(conta_Comum_1.calculoRendimento())
+    print(conta_comum_1.calculoRendimento())
     print(conta_vip_1.calculoRendimento())
 
 
